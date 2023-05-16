997a12f2bf6dee2b77096e61ae8e06731d9b8007
wayne <wayne.wan-chow-wah@etu.umontpellier.fr>
2023-05-16 13:21:30 +0200

    add routes to App.js

--- src/app.js
+++ src/app.js
@@ -4,24 +4,39 @@ const express = require("express")
 
 const helmet = require("helmet")
 
-const productRoutes = require("./routes/productRoutes")
+const exampleRoute = require("./routes/exampleRoute");
+const carItemRoutes = require("./routes/carItemRoutes");
+const orderDetailsRoutes = require("./routes/orderDetailsRoutes");
+const orderItemsRoutes = require("./routes/orderItemsRoutes");
+const productRoutes = require("./routes/productRoutes");
+const shoppingSessionRoutes = require("./routes/shoppingSessionRoutes");
+const userAddressRoutes = require("./routes/userAddressRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express()
 
 //app.use(helmet())
 
 app.use(express.json())
-
 app.use(express.urlencoded({extended: true}))
 
+app.use("/example", exampleRoute)
+app.use("/carItem", carItemRoutes)
+app.use("/orderDetails", orderDetailsRoutes)
+app.use("/orderItems", orderItemsRoutes)
 app.use("/product", productRoutes)
+app.use("/shoppingSession", shoppingSessionRoutes)
+app.use("/userAddress", userAddressRoutes)
+app.use("/user", userRoutes)
+
+/*
 
 app.get("/", (req, res)=>{
     let uptimeObj = {uptime: process.uptime()}
     let uptimeStr = JSON.stringify(uptimeObj)
     res.status(200).send(uptimeStr)
-})
+});
 
+*/
 
-
-module.exports = app
+module.exports = app;
