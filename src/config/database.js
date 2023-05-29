aad16b8d32a08422717ba2e62ed16fefcb713ad5
wayne <wayne.wan-chow-wah@etu.umontpellier.fr>
2023-05-29 16:17:19 +0200

    deploy test

--- src/config/database.js
+++ src/config/database.js
@@ -4,13 +4,32 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config()
 //console.log(process.env.DATABASE_URL)
 
+/*
 const sequelize = new Sequelize(
   process.env.DATABASE_URL,
   {
     logging: false,
   }
-)
+*/
 
+const sequelize = new Sequelize(
+  process.env.PG_NAME,
+  process.env.PG_USERNAME,
+  process.env.PG_PASSWORD,
+  {
+      host: process.env.PG_HOST,
+      dialect: "postgres",
+      dialectOptions: {
+          ssl: {
+              require: true,
+              rejectUnauthorized: false
+          }
+      },
+      port: process.env.PG_PORT,
+      logging: false
+  }
+  
+);
 
 /*
 
